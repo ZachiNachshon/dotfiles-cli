8d38ad43e46cd54f7d2b52f2aad86d6258f2fae2
ZachiNachshon <zachi.nachshon@gmail.com>
2022-08-07 09:21:53 +0300

    Reload session header on RC file is removed from install script to reload command
    Added a Homebrew formula backup file

---
+++ formula.rb
@@ -0,0 +1,23 @@
+# typed: false
+# frozen_string_literal: true
+
+class DotfilesCli < Formula
+  desc "CLI utility for managing a remote dotfiles repository"
+  homepage "https://github.com/ZachiNachshon/dotfiles-cli"
+  version "0.2.0"
+  url "https://github.com/ZachiNachshon/dotfiles-cli/releases/download/v0.2.0/dotfiles-cli.tar.gz"
+  sha256 "0ce66da6a2aee37e11b2a7cf9292fe0b446195ba0febc7c29da541e4e4cc8ed4"
+  license "MIT"
+
+  depends_on "git"
+
+  def install
+    # Add the dotfiles script header to RC file (zsh, bashrc, bash_profile)
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"dotfiles.sh" => "dotfiles"
+  end
+  
+  test do
+    system "#{bin}/dotfiles version"
+  end
+end
