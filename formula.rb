bf0ad1d515971b7d9dfc969d0e559416a309a953
ZachiNachshon <zachi.nachshon@gmail.com>
2022-08-13 21:22:54 +0300

    Ignore test data from GitHub linguist

--- formula.rb
+++ formula.rb
@@ -6,14 +6,15 @@ class DotfilesCli < Formula
   homepage "https://github.com/ZachiNachshon/dotfiles-cli"
   version "0.3.0"
   url "https://github.com/ZachiNachshon/dotfiles-cli/releases/download/v0.3.0/dotfiles-cli.tar.gz"
-  sha256 "0ce66da6a2aee37e11b2a7cf9292fe0b446195ba0febc7c29da541e4e4cc8ed4"
+  sha256 "a11869e49d4667bb6ab4a58c4bdd98d7a957321210b7513225548e03d454be7d"
   license "MIT"
 
   depends_on "git"
 
   def install
-    # Add the dotfiles script header to RC file (zsh, bashrc, bash_profile)
+    # Add extracted files to the Homebrew install directory
     libexec.install Dir["*"]
+    # Add a relative symlink from Homebrew libexec to bin folder
     bin.install_symlink libexec/"dotfiles.sh" => "dotfiles"
   end
   
